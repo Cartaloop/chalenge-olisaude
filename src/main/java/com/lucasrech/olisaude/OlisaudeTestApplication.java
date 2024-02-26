68aaff9eaa5cabf821d799810fa6407e5f8549d6
Lucas <lucasrech00@gmail.com>
2024-02-26 00:35:40 -0300

    Initial commit

---
+++ src/main/java/com/lucasrech/olisaude/OlisaudeTestApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrech.olisaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlisaudeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlisaudeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasrech/olisaude/OlisaudeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasrech.olisaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlisaudeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
